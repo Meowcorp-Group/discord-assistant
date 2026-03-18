59565b81161ef9e8351948d491127f3d1a38bd88
Waoweens <30313089+Waoweens@users.noreply.github.com>
2026-03-18 14:35:24 +0700

    bot basic

--- bot/index.ts
+++ bot/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers
+	]
+})
+
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`)
+})
+
+client.on(Events.MessageCreate, (message) => {
+	if (message.author.bot) return
+	if (message.content.trim().startsWith(`<@${client.user?.id}>`)) {
+		message.reply('meow')
+	}
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
