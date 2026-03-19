489d23b86321635a1e7125b2dee4eae82297cb06
Waoweens <30313089+Waoweens@users.noreply.github.com>
2026-03-19 19:08:43 +0700

    grid icon

---
+++ activity/src/lib/components/GridIcon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let { columns, rows }: { columns: number; rows: number } = $props();
+
+	// svelte-ignore state_referenced_locally
+	const cells = Array.from({ length: columns * rows }, () => Math.random() < 0.2);
+</script>
+
+<div class="grid w-full aspect-square" style={`grid-template-columns: repeat(${columns}, 1fr);`}>
+	{#each cells as active}
+		<div class="border" class:bg-base-content={active}></div>
+	{/each}
+</div>
