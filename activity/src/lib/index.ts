5bcd47a39b45d95bd8d1f0a37d1c57a3701b9f14
Waoweens <30313089+Waoweens@users.noreply.github.com>
2026-03-19 19:09:03 +0700

    map value, get date util functions

--- activity/src/lib/index.ts
+++ activity/src/lib/index.ts
@@ -1,1 +1,16 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import { error } from "@sveltejs/kit";
+import { Temporal } from "temporal-polyfill";
+
+export function mapValue<T extends string | number | symbol, R>(mapping: Record<T, R>, value: T): R {
+	if (!(value in mapping)) {
+		throw error(400, `Invalid value: ${String(value)}. Expected one of: ${Object.keys(mapping).join(', ')}`);
+	}
+	return mapping[value];
+}
+
+export function getDaysAgoDate(timezone: string, days: number): string {
+	const today = Temporal.Now.zonedDateTimeISO(timezone).toPlainDate();
+	return today.subtract({ days }).toString();
+}
